1705654566f8954010ae9fe3da341995121d6cd8
Bill Glover <bill@billglover.co.uk>
2016-06-11 15:17:20 +0100

    Initialise repository

---
+++ location-tracker/location-tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  location-tracker
+//
+//  Created by Bill Glover on 11/06/2016.
+//  Copyright © 2016 Bill Glover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
